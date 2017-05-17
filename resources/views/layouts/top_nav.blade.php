8825804e792b643fb082a20a88c81b3e84a66db9
julleerizzsenas <mayx12th@gmail.com>
2017-05-17 15:43:56 +0800

    modify topnav with username

--- resources/views/layouts/top_nav.blade.php
+++ resources/views/layouts/top_nav.blade.php
@@ -7,24 +7,37 @@
         <a id="menu_toggle"><i class="fa fa-bars"></i></a>
 
       </div>
-
+      
       <ul class="nav navbar-nav navbar-right">
-        <li class="">
-          <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-            <img src="../images/download.png" alt="">John Doe
-            <span class=" fa fa-angle-down"></span>
+        @if (Auth::guest())
+          <li><a href="{{ route('login') }}">Login</a></li>
+          <li><a href="{{ route('register') }}">Register</a></li>
+        @else<li class="dropdown">
+        <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+          <img src="../images/download.png" alt="">
+            {{ Auth::user()['name'] }}
+          <span class=" fa fa-angle-down"></span>
+        </a>
+        <ul class="dropdown-menu dropdown-usermenu pull-right">
+          <li><a href="javascript:;"> Profile</a></li>
+
+          <li><a href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+            Logout
           </a>
-          <ul class="dropdown-menu dropdown-usermenu pull-right">
-            <li><a href="javascript:;"> Profile</a></li>
-            
-            <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-          </ul>
-        </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-  </div>
+
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            {{ csrf_field() }}
+          </form></li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+  @endif
+</ul>
+</nav>
+</div>
 </div>
 
 <!-- /top navigation -->
